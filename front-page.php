7b206fe4eeacec59d4602fc671afc2e503fa3005
unknown <chikadze.tamo@gmail.com>
2024-05-18 20:01:31 +0400

    first commit

---
+++ front-page.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+
+
+<main class="home-page">
+<section class="home-section">
+<div class="content">
+<p class="content-hello"> Hello </p>
+<h1 class="content-title">
+I’m Akhil T J
+</h1>
+<p class="content-text">
+Freelance Designer, specialized in UI/UX.
+</p>
+<div class="content-social">
+    <a href="#">
+        <img src=<?php echo get_template_directory_uri() . "./assets/images/twitter.png"  ?> alt="">
+    </a>
+    <a href="#">
+    <img src=<?php echo get_template_directory_uri() . "./assets/images/linkedin.png"  ?> alt="">
+    </a> <a href="#">
+    <img src=<?php echo get_template_directory_uri() . "./assets/images/instagram.png"  ?> alt="">
+    </a> <a href="#">
+    <img src=<?php echo get_template_directory_uri() . "./assets/images/behance.png"  ?> alt="">
+   
+
+
+</div>
+</div>
+
+
+</section>
+</main>
