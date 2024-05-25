6e887605853b6445cca283b342b275c2b75854ac
unknown <chikadze.tamo@gmail.com>
2024-05-25 17:19:23 +0400

    contact

--- front-page.php
+++ front-page.php
@@ -27,7 +27,29 @@ Freelance Designer, specialized in UI/UX.
 
 </div>
 </div>
-
-
+\
 </section>
 </main>
+
+<section class="contact-page">
+<div class="contact">
+<?php echo do_shortcode ('[contact-form-7 id="6b013b8" title="Contact form 1" html_class="portfolio-contact-form"]');
+
+?>
+</div>
+<div class="content-social">
+    <a href="#">
+        <img src=<?php echo get_template_directory_uri() . "./assets/images/twitter.png"  ?> alt="">
+    </a>
+    <a href="#">
+    <img src=<?php echo get_template_directory_uri() . "./assets/images/linkedin.png"  ?> alt="">
+    </a> <a href="#">
+    <img src=<?php echo get_template_directory_uri() . "./assets/images/instagram.png"  ?> alt="">
+    </a> <a href="#">
+    <img src=<?php echo get_template_directory_uri() . "./assets/images/behance.png"  ?> alt="">
+   
+
+
+</div>
+
+</section>
